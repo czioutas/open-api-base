795258f1492005e13e54f5d6137287ec7e45ed0b
Chris Zioutas <z.chris.92@gmail.com>
2023-08-31 19:20:30 +0200

    change migration

--- core/src/migrations/1693502199406-ma.ts
+++ core/src/migrations/1693502199406-ma.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class M1693427409748 implements MigrationInterface {
-    name = 'M1693427409748';
+export class Ma1693502199406 implements MigrationInterface {
+    name = 'Ma1693502199406'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "user_entity" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP(3) NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT now(), "firstName" character varying, "lastName" character varying, "email" character varying NOT NULL, CONSTRAINT "PK_b54f8ea623b17094db7667d8206" PRIMARY KEY ("id"))`);
